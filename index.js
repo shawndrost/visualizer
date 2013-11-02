801e57657f1e71a048b5efe4c800d8710ad2f7e7
Shawn Drost <sdrost@gmail.com>
2013-11-01 18:54:21 -0700

    three.js boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+var scene, camera, renderer, container, sceneSize;
+
 function init () {
+  sceneSize = 100;
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    sceneSize / 1000,
+    sceneSize * 20
+  );
+  camera.position.z = sceneSize;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  container = document.createElement('div');
+  document.body.appendChild(container);
+  container.appendChild(renderer.domElement);
+
   animate();
 }
 
 function animate () {
   window.requestAnimationFrame(animate); // ensures this function gets called once every 60 seconds
+  renderer.render(scene, camera);
 }
 
 init();
